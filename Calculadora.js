0014a89b2258831e51a92effd0df9f5bed5a3253
dani <421083671@pcpuma.acatlan.unam.mx>
2023-07-22 19:50:40 -0600

    Conectar index.html con scripts js

---
+++ Calculadora.js
@@ -0,0 +1,18 @@
+//El nombre de este archivo empieza con mayúscula pues contendrá una clase
+class Calculadora{
+    sumar(num1, num2){
+        return num1 + num2;
+    }
+
+    restar(num1, num2){
+        return num1 - num2;
+    }
+
+    multiplicar(num1, num2){
+        return num1 * num2;
+    }
+
+    dividir(num1, num2){
+        return num1 / num2;
+    }
+}
